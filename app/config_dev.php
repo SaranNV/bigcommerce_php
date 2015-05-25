1ef3257d99bd7326b282c73f1d54dad7047873df
SaravananNV <saravanannagarajannv@gmail.com>
2015-05-25 12:44:36 +0530

    wombat api key and wombat token

--- app/config_dev.php
+++ app/config_dev.php
@@ -10,7 +10,7 @@ $app['bc_api_base'] 		= "https://api.bigcommerce.com";
 //Wombat authentication tokens
 $app['wombat_api_base'] = "https://push.wombat.co";
 $app['wombat_tokens'] 	= array(
-	'53cffa32776f6d6e34740000' => 'db4ebe193caad7c75589bc9a865a4d6925dad15f50e4ca69', //rodeck demo store
+	'5562c914736d6126e3a00100' => 'f988499b6d65a0c7b98f0589ff39cd846dc7be62eb96d69f', //rodeck demo store
 	);
 // $app['wombat_store']		= "53cffa32776f6d6e34740000";
 // $app['wombat_token']		= "db4ebe193caad7c75589bc9a865a4d6925dad15f50e4ca69";
